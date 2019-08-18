c8afadec430e10bcb554f79bd6f192a2f0e94f67
sameer <syedjava28@gmail.com>
2019-08-18 07:01:39 -0400

    spring boot jpa swagger

---
+++ src/main/java/com/learn/jpa/swagger/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.learn.jpa.swagger.repository;
+
+import com.learn.jpa.swagger.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+}
